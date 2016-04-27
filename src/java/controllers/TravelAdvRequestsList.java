7054872c13bcea6a069e88557e8cda7721dcdae4
Kelli Muhindi <muhindi09@gmail.com>
2016-04-27 12:53:48 +0300

    almost done on travel request details view

--- src/java/controllers/TravelAdvRequestsList.java
+++ src/java/controllers/TravelAdvRequestsList.java
@@ -8,13 +8,19 @@ package controllers;
 import entities.TravelAdvanceRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.transaction.UserTransaction;
 import mediators.TravelAdvanceRequestJpaController;
+import org.primefaces.event.SelectEvent;
 
 /**
  *
@@ -33,14 +39,15 @@ public class TravelAdvRequestsList {
     private UserTransaction ut;
     private List<TravelAdvanceRequest> tar;
     
+    @PostConstruct
     public void init(){
         travelAdvRequest = new TravelAdvanceRequest();
         jpaController = new TravelAdvanceRequestJpaController(ut, emf);
+        tar = new ArrayList<>();
         getListing();
     }
     
     public TravelAdvRequestsList() {
-       tar = new ArrayList<>();
     }
 
     public List<TravelAdvanceRequest> getTar() {
@@ -61,5 +68,12 @@ public class TravelAdvRequestsList {
     
     private  void getListing(){
         tar = jpaController.findTravelAdvanceRequestEntities();
+    }
+    
+    public void onRowSelect(SelectEvent event){
+//        Object key = FacesContext.getCurrentInstance().getAttributes().get("res");
+        FacesMessage fmsg = new FacesMessage("Detected", ((TravelAdvanceRequest)event.getObject()).getDestination());
+        Logger.getAnonymousLogger().log(Level.INFO, fmsg.getDetail());
+        FacesContext.getCurrentInstance().addMessage("messenger", fmsg);
     }
 }
