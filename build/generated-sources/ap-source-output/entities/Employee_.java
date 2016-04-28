b7564e6371aead82352d4d65eecfe3a6ae6bc345
Kelli Muhindi <muhindi09@gmail.com>
2016-04-28 15:39:02 +0300

    Working branch issues resolved

--- build/generated-sources/ap-source-output/entities/Employee_.java
+++ build/generated-sources/ap-source-output/entities/Employee_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-28T15:08:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-28T15:37:06")
 @StaticMetamodel(Employee.class)
 public class Employee_ { 
 
--- build/generated-sources/ap-source-output/entities/Item_.java
+++ build/generated-sources/ap-source-output/entities/Item_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-28T15:08:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-28T15:37:06")
 @StaticMetamodel(Item.class)
 public class Item_ { 
 
